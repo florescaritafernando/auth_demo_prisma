685186ca5c867829ac4060b531df747f4da109a7
fernandoflores <florescaritafernando@gmail.com>
2026-05-07 13:29:49 -0500

    adapter neon db para cloudflare

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from "@/lib/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 const connectionString = process.env.DATABASE_URL!.includes("sslmode") 
     ? process.env.DATABASE_URL!
     : process.env.DATABASE_URL! + (process.env.DATABASE_URL!.includes("?") ? "&" : "?") + "sslmode=verify-full";
 
-const adapter = new PrismaPg({
-    connectionString,
-});
+// @ts-expect-error - El adapter de Neon tiene problemas de tipos con Prisma v7
+const adapter = new PrismaNeon(connectionString);
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
@@ -18,9 +17,6 @@ const prisma =
     new PrismaClient({
         adapter,
     });
-
-// Ensure all models are loaded
-prisma.$connect().catch(console.error);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
